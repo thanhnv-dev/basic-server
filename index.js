5ca60331238bfd641f2f0e24c6b81edb2f9ae71b
thanhnv2 <thanhnv2@newwave.vn>
2024-08-07 09:33:38 +0700

    update api doc

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const db = require('./src/configs/db.config.js');
 const apiRoute = require('./src/routes/router.js');
 const {PORT} = require('./src/constants/index.js');
 const admin = require('firebase-admin');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const YAML = require('yaml');
 const {SERVICE_ACCOUNT, STORAGE_BUCKET} = require('./src/constants/index.js');
 
 db.connect().then(() => {
@@ -14,11 +17,13 @@ db.connect().then(() => {
   const app = express();
 
   app.use(cors());
-
   app.use(express.urlencoded({extended: true}));
-
   app.use(express.json());
 
+  const file = fs.readFileSync('./api_docs.yaml', 'utf8');
+  const swaggerDocument = YAML.parse(file);
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use('/', apiRoute);
 
   app.listen(PORT, () => {
