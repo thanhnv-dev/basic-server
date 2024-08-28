53fa226f8ba0219f9964ec1ac20ed3f21d490cb5
thanhnv2 <thanhnv2@newwave.vn>
2024-08-28 09:28:32 +0700

    Hot fix

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -266,7 +266,7 @@ const deleteDeliveryAddress = async req => {
 
 const updateInfomations = async req => {
   const token = JWToken.getTokenFromRequest(req);
-
+  const {user_name, email, date_of_birth, gender, phone_number} = req.body;
   const data = JWToken.decodedToken(token);
 
   const id = data?.payload?.id;
@@ -275,49 +275,63 @@ const updateInfomations = async req => {
 
   if (findUserByIdResult) {
     const currenEmail = findUserByIdResult.email;
-    const {user_name, email, date_of_birth, gender, phone_number} = req.body;
 
-    if (!(gender === 'Male' || gender === 'Female')) {
-      return {
-        status: 400,
-        res: {msg: '""gender" must be Male or Female'},
-      };
-    }
-    const updateAvatarRes = await UserModel.updateMany(
-      {
-        _id: id,
-      },
-      {
-        user_name: user_name,
-        date_of_birth: date_of_birth,
-        gender: gender,
-        phone_number: phone_number,
-        email: email,
-        is_verified_email: email === currenEmail,
-      },
-    );
+    const findUserByEmailResult = await findUserByEmail(currenEmail);
 
-    if (updateAvatarRes.acknowledged) {
-      const userDataNew = await findUserById(id);
-
-      if (userDataNew) {
-        const resDataUser = userDataNew.toObject();
-        delete resDataUser.createdAt;
-        delete resDataUser.updatedAt;
-        delete resDataUser.address_default;
-        delete resDataUser.password;
-
+    if (findUserByEmailResult && email !== currenEmail) {
+      return {status: 400, res: {msg: 'Email was registered!'}};
+    } else {
+      if (!(gender === 'Male' || gender === 'Female')) {
         return {
-          status: 200,
-          res: {
-            results: resDataUser,
-            msg: 'Update informations successfully!',
-          },
+          status: 400,
+          res: {msg: '""gender" must be Male or Female'},
         };
       }
-    }
+      const updateAvatarRes = await UserModel.updateMany(
+        {
+          _id: id,
+        },
+        {
+          user_name: user_name,
+          date_of_birth: date_of_birth,
+          gender: gender,
+          phone_number: phone_number,
+          email: email,
+        },
+      );
+      if (email !== currenEmail) {
+        await UserModel.updateMany(
+          {
+            _id: id,
+          },
+          {
+            is_verified_email: false,
+          },
+        );
+      }
 
-    return {status: 200, res};
+      if (updateAvatarRes.acknowledged) {
+        const userDataNew = await findUserById(id);
+
+        if (userDataNew) {
+          const resDataUser = userDataNew.toObject();
+          delete resDataUser.createdAt;
+          delete resDataUser.updatedAt;
+          delete resDataUser.address_default;
+          delete resDataUser.password;
+
+          return {
+            status: 200,
+            res: {
+              results: resDataUser,
+              msg: 'Update informations successfully!',
+            },
+          };
+        }
+      }
+
+      return {status: 200, res};
+    }
   } else {
     return {
       status: 403,
